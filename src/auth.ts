6c834dd7d4cd99ef5c1c519656b5dfba3e0db819
venlomj <venlo.mj@hotmail.nl>
2024-08-10 21:44:35 +0200

    End of section 3

---
+++ src/auth.ts
@@ -0,0 +1,23 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import authConfig from "./auth.config";
+import { prisma } from "./lib/prisma";
+
+export const {
+  auth,
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+} = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
