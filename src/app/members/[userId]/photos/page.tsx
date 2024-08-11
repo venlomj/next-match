7784c7111638a6160059f718f1ccffa607f5945b
venlomj <venlo.mj@hotmail.nl>
2024-08-11 22:04:33 +0200

    End of section 4

---
+++ src/app/members/[userId]/photos/page.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { CardBody, CardHeader, Divider, Image } from "@nextui-org/react";
+import { getMemberPhotoByUserId } from "@/app/actions/memberActions";
+
+export default async function PhotosPage({
+  params,
+}: {
+  params: { userId: string };
+}) {
+  const photos = await getMemberPhotoByUserId(params.userId);
+  return (
+    <>
+      <CardHeader className="text-2xl font-semibold text-secondary">
+        Photos
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        <div className="grid grid-cols-5 gap-3">
+          {photos &&
+            photos?.map((photo) => (
+              <div key={photo.id}>
+                <Image
+                  width={300}
+                  height={300}
+                  src={photo.url}
+                  alt="Image of member"
+                  className="object-cover aspect-square"
+                />
+              </div>
+            ))}
+        </div>
+      </CardBody>
+    </>
+  );
+}
